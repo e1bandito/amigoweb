7f900951d846ba94b289d7f567489327ec4d6d4a
e1bandito <aleksandr.shitov@mail.ru>
2021-01-29 13:31:18 +0300

    add components

---
+++ src/components/Form.vue
@@ -0,0 +1,26 @@
+<template>
+  <form action="" class="form"></form>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<style lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
